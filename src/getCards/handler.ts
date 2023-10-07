873c46ee3d896afffc12fb0b6513b0bdd8d85d1c
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2023-10-07 17:05:43 +0100

    Create /cards endpoint

---
+++ src/getCards/handler.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import axios from 'axios';
+import { Card, Page, ResultCard } from "../interfaces/card";
+import { Template } from "../interfaces/template";
+
+export const CARDS_URL = 'https://moonpig.github.io/tech-test-node-backend/cards.json';
+export const TEMPLATES_URL = 'https://moonpig.github.io/tech-test-node-backend/templates.json';
+
+export const getCardsHandler = async (req: Request, res: Response) => {
+  try {
+    // Requests are made in parallel
+    const [cardsResponse, templatesResponse] = await Promise.all([
+      axios.get<Card[]>(CARDS_URL),
+      axios.get<Template[]>(TEMPLATES_URL)
+    ]);
+
+    const cards = cardsResponse.data;
+    const templates = templatesResponse.data;
+
+    if (cards.length === 0) {
+      // If the card does not exist, send a 404 response
+      res.status(404).json({ error: 'No cards found' });
+      return;
+    }
+
+    const result: ResultCard[] = cards.map((card: Card) => {
+      // For each card, look for the page that has a title of 'front cover'.
+      const frontCover = card.pages.find((page: Page) => page.title === 'Front Cover');
+      /* 
+      For each template, look for the template that has the same id as the
+      frontCover template id, and fetch the imageUrl associated with this 
+      template id.
+      */
+      const imageUrl = templates.find((template: Template) => template.id === frontCover.templateId).imageUrl;
+      
+      return {
+        title: card.title,
+        imageUrl,
+        url: `/cards/${card.id}`
+      };
+    });
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
---
+++ src/interfaces/template.ts
@@ -0,0 +1,6 @@
+export interface Template {
+  id: string;
+  width: number;
+  height: number;
+  imageUrl: string;
+}
